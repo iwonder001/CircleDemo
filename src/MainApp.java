131fadba9c6acb35c940484732150dfd0a3a6a26
Kristen <kcarethers@gmail.com>
2016-12-15 19:48:44 -0500

    Initial commit

---
+++ src/MainApp.java
@@ -0,0 +1,15 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	// Create Circle class with radius and PI fields
+	public class Circle {
+		private double radius;
+		private double pi;
+	}
+
+}// close MainApp class
