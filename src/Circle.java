366264692681bf2b999342f66d045e86b79ccd78
Kristen <kcarethers@gmail.com>
2016-12-15 20:47:40 -0500

    Wrote getRadius method

---
+++ src/Circle.java
@@ -0,0 +1,17 @@
+// Create Circle class with radius and PI fields
+public class Circle {
+
+	private double radius;
+	private final double PI = 3.14159;
+
+	/**
+	 * The setRadius method stores a value in the length field.
+	 * @param rad The value to store in radius.
+	 */
+	public void setRadius(double rad) {
+		radius = rad;
+	}
+	public double getRadius(){
+		return radius;
+	}
+}//close class Circle
